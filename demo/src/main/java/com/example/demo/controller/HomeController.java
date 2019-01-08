982db1a2588af4847b84759072c812028f0a39d5
lucien.moor <lucien.moor@he-arc.ch>
2019-01-08 09:24:55 +0100

    init

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    
+    @GetMapping("/add")
+    @ResponseBody
+    public String add() {
+    	
+        return Integer.toString(addi());
+    }
+    
+    private int addi() {
+    	return 2+2;
+    }
+}
